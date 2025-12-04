da54f52b9cc29ce2dd19b2aec9a5731f3f240f7b
BASSprogrammer <bariassalvo@gmail.com>
2025-12-03 21:22:40 -0300

    Create  portfolio web - react ts

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import './index.css'
+import { Header } from './components/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header title="Mi Portafolio Web" />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+interface HeaderProps {
+  title: string;
+}
+
+export const Header = ({ title }: HeaderProps) => {  return (
+    <header className="bg-blue-500 text-white p-4">
+      <h1 className="text-2xl font-bold">{title}</h1>
+    </header>
+  );
+};
