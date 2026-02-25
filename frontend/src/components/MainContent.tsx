2cffe3b68828fe6ebdaedffd4d075b4bb69dae9f
BAASprogrammer <bariassalvo@gmail.com>
2026-02-25 17:25:26 -0300

    fix: remove unknown compiler option and reformat MainContent

--- frontend/src/components/MainContent.tsx
+++ frontend/src/components/MainContent.tsx
@@ -6,15 +6,16 @@ import { Skills } from '../pages/Skills';
 import { PersonalIntro } from '../pages/PersonalIntro';
 import { Footer } from './Footer';
 
-export const MainContent = () => {  return (
-    <div className="text-default p-4 sm:p-6 lg:p-0 mx-auto w-full max-w-3xl text-left">
-      <PersonalIntro/>
-      <About id = "about"/>
-      <Experience id='experience'/>
-      <Projects id='projects'/>
-      <Skills id='skills'/>
-      <ContactForm id='contact'/>
-      <Footer/>
+export const MainContent = () => {
+  return (
+    <div className="text-default p-4 sm:p-6 mx-auto w-full max-w-4xl text-left">
+      <PersonalIntro />
+      <About id="about" />
+      <Experience id='experience' />
+      <Projects id='projects' />
+      <Skills id='skills' />
+      <ContactForm id='contact' />
+      <Footer />
     </div>
   );
 };
